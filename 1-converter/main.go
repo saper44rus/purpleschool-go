b40a8bad4d43e038e370d7fde502c2df333f2a90
Aleksandr Kozlov <kozlov.a@vms.com.ru>
2024-07-18 03:41:19 +0300

    Done amount currency

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
 
 func main() {
 
@@ -8,18 +12,63 @@ func main() {
 		usdToEurRate = 0.93
 		usdToRubRate = 88.50
 	)
-	eurToRubRate := usdToRubRate / usdToEurRate
-	fmt.Println(eurToRubRate)
-	fmt.Println(readUserParams())
+
+	var sourceCurrency string
+	var err error
+
+	for {
+		sourceCurrency, err = choiseSourceCurrency()
+		if err != nil {
+			fmt.Printf("Ошибка: %s\n", err)
+		} else {
+			fmt.Printf("Исходная валюта: %s\n", sourceCurrency)
+			break
+		}
+	}
+
+	for {
+		amount, err := amountCurrency()
+		if err != nil {
+			fmt.Println("Ошибка:", err) // Это сообщение не должно выводиться, так как функция не возвращает ошибку
+		} else {
+			fmt.Println("Введенная сумма: ", amount)
+			break
+		}
+
+	}
+
 }
 
-func readUserParams() string {
-    var userData string
-    fmt.Print("Enter your data: ")
-    fmt.Scanln(&userData)
-	return userData
+func choiseSourceCurrency() (string, error) {
+	for {
+		var input string
+		fmt.Print("Выберете валюту (USD, EUR, RUB): ")
+		fmt.Scan(&input)
+		// Проверка допустимости валюты
+		if input == "USD" || input == "EUR" || input == "RUB" {
+			return input, nil // Валюта допустима
+		} else {
+			return "", errors.New("Неправильная валюта. Попробуйте еще раз.")
+			// fmt.Println("Неправильная валюта. Попробуйте еще раз.")
+		}
+	}
 }
 
-func currencyCalculation(summCurrency int,sourceCurrency, targetCurrency string) float64 {
+func amountCurrency() (float64, error) {
+	for {
+		var input string
+		for {
+			fmt.Print("Введите сумму: ")
+			fmt.Scanln(&input)
 
+			amount, err := strconv.ParseFloat(input, 64)
+			if err == nil {
+				return amount, nil
+			} else {
+				return 0, errors.New("Введенное значение не является числом. Попробуйте еще раз.")
+			}
+
+		}
+
+	}
 }
